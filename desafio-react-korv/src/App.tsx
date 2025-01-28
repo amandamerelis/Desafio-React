cbc64025e9ab8e2b29de5fbe57bf07c872650b6e
Amanda Merelis <amanda.merelis@basisti.com.br>
2025-01-28 18:40:25 -0300

    feat: criação do AppRoutes

--- desafio-react-korv/src/App.tsx
+++ desafio-react-korv/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 import {ThemeProvider} from "@mui/material";
 import {LightTheme} from "./themes";
+import {AppRoutes} from "./routes";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
 
     return (
         <ThemeProvider theme={LightTheme}>
+            <BrowserRouter>
+                <AppRoutes></AppRoutes>
+            </BrowserRouter>
         </ThemeProvider>
     )
 }
