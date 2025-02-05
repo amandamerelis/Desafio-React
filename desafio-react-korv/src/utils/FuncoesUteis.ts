dbc8d253796bf84c3cb495b0fcb9c9e7c798f1dc
Amanda Merelis <amanda.merelis@basisti.com.br>
2025-02-05 12:50:35 -0300

    refactor: mudança na formatação de data tipo 'medio'

--- desafio-react-korv/src/utils/FuncoesUteis.ts
+++ desafio-react-korv/src/utils/FuncoesUteis.ts
@@ -2,20 +2,31 @@ type FormatoData = 'curto' | 'medio' | 'longo';
 
 export function formatarData(data: Date, formato: FormatoData): string {
 
+    const formatarMedio = (data: Date) => {
+        const partes = new Intl.DateTimeFormat('pt-BR', {
+            day: 'numeric',
+            month: 'short',
+            year: '2-digit'
+        }).formatToParts(data);
+
+        const dia = partes.find(p => p.type === 'day')?.value;
+        const mes = partes.find(p => p.type === 'month')?.value;
+        const ano = partes.find(p => p.type === 'year')?.value;
+
+        return `${dia} ${mes} ${ano}`;
+    };
+
     switch (formato) {
     case 'curto':
         return Intl.DateTimeFormat('pt-BR', {
             day: 'numeric',
             month: 'short',
-            year: '2-digit'
+            year: '2-digit',
+
         }).format(data);
 
     case 'medio':
-        return Intl.DateTimeFormat('pt-BR', {
-            day: 'numeric',
-            month: 'short',
-            year: '2-digit'
-        }).format(data);
+        return formatarMedio(data);
 
     case 'longo':
         return Intl.DateTimeFormat('pt-BR', {
