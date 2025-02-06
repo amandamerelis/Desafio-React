902a9dad71177e0b3a4078306b3f86346235d1ef
Amanda Merelis <amanda.merelis@basisti.com.br>
2025-02-06 16:49:13 -0300

    fix: correção do componente de info de usuário para usar o componente de avatar

--- desafio-react-korv/src/components/usuarios/InfoUsuarioLogado.tsx
+++ desafio-react-korv/src/components/usuarios/InfoUsuarioLogado.tsx
@@ -12,15 +12,14 @@ interface InfoUsuarioLogadoProps {
 const InfoUsuarioLogado = ({ usuario }: InfoUsuarioLogadoProps) => {
     const [expanded, setExpanded] = useState(false);
     const theme = useTheme();
-    const tamanho = parseInt(theme.spacing(12));
 
     return (
         <Box width={theme.spacing(70)} height={theme.spacing(20)} paddingX={theme.spacing(2)}
             display="flex" alignItems="center" justifyContent="space-between">
             <Box display="flex" alignItems="center" gap={theme.spacing(2)}>
                 {usuario.urlFoto
-                    ? <AvatarComFoto altura={tamanho} largura={tamanho} urlFoto={usuario.urlFoto}/>
-                    : <AvatarComIniciais altura={tamanho} largura={tamanho} nome={usuario.nome}/>}
+                    ? <AvatarComFoto tamanho="grande" estilo="rounded" urlFoto={usuario.urlFoto} />
+                    : <AvatarComIniciais tamanho="grande" estilo="rounded" nome={usuario.nome}/>}
                 <Typography variant="body1">{usuario.nome}</Typography>
             </Box>
 
